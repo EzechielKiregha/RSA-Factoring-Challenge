bbf3994f9c1ab34e345ccee642247221940a72ee
EzechielKiregha <ekiregha@gmail.com>
2024-01-19 15:00:07 -0800

    .......... Factorize all the things!

---
+++ factors.cpp
@@ -0,0 +1,17 @@
+#include "header.h"
+
+/**
+ * main - checks the entry point of the program
+ * @ac: argument counter integer variable
+ * @av: argument vector, a char array
+ * Return: Always 0
+ */
+int main(__attribute__((unused)) int ac, char **av)
+{
+	RSARef fact(av[1]);
+
+	fact.run();
+
+	return (0);
+}
+
